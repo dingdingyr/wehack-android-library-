dbf49939033a375752a560a5fcdaf232dab9b158
androiddev <zhichangterry@gmail.com>
2014-12-10 07:18:27 +0800

    density_util

---
+++ src/main/java/cn/wehax/library/utils/DensityUtil.java
@@ -0,0 +1,15 @@
+package cn.wehax.library.utils;
+
+import android.app.Activity;
+
+/**
+ * Created by Terry on 14/12/10.
+ * mail: zhichangterry@gmail.com
+ * QQ: 1090035354
+ */
+public class DensityUtil {
+    public static int dp2px(Activity activity,int value){
+        //TODO: 实现
+        return 0;
+    }
+}
