2a88be374ea005c82aacf74b2d271055479aefe1
aguan <clearnsee@gmail.com>
2015-01-05 11:10:24 +0800

    -

--- src/main/java/cn/wehax/common/framework/data/RequestManager.java
+++ src/main/java/cn/wehax/common/framework/data/RequestManager.java
@@ -7,6 +7,8 @@ import com.android.volley.toolbox.Volley;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
+import javax.inject.Inject;
+
 /**
  * Created by Terry on 14/12/24.
  * mail: zhichangterry@gmail.com
@@ -18,7 +20,8 @@ public class RequestManager {
     Application mApplication;
     private RequestQueue mRequestQueue;
 
-    RequestManager(Provider<Application> provider){
+    @Inject
+    RequestManager(Provider<Application> provider) {
         mApplication = provider.get();
     }
 
