8e9fc042ccf609ece64e60cab6fb71c30f7b166a
howejee <366201633@qq.com>
2015-01-09 10:17:36 +0800

    --

--- src/main/java/cn/wehax/util/ImageUtil.java
+++ src/main/java/cn/wehax/util/ImageUtil.java
@@ -5,6 +5,12 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.widget.Toast;
@@ -193,5 +199,39 @@ public class ImageUtil {
         options.inJustDecodeBounds = false;
 
         return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_4444);
+        Canvas canvas = new Canvas(output);
+
+        final Paint paint = new Paint();
+        //保证是方形，并且从中心画
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int w;
+        int deltaX = 0;
+        int deltaY = 0;
+        if (width <= height) {
+            w = width;
+            deltaY = height - w;
+        } else {
+            w = height;
+            deltaX = width - w;
+        }
+        final Rect rect = new Rect(deltaX, deltaY, w, w);
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        //圆形，所有只用一个
+
+        int radius = (int) (Math.sqrt(w * w * 2.0d) / 2);
+        canvas.drawRoundRect(rectF, radius, radius, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
     }
 }
