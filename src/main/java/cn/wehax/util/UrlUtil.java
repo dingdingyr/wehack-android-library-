bb8e3a4bb44708e55fe40945ab8f6833e57fc0bf
Terry Wang <zhichangterry@gmail.com>
2015-01-16 13:29:37 +0800

    +++

---
+++ src/main/java/cn/wehax/util/UrlUtil.java
@@ -0,0 +1,35 @@
+package cn.wehax.util;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by howe on 15/1/14.
+ * Email:howejee@gmail.com
+ */
+public class UrlUtil {
+    public static String assembleUrlByGet(String url, Map<String, String> params) {
+        Iterator<Map.Entry<String, String>> iterator = params.entrySet().iterator();
+        StringBuilder sb = new StringBuilder(url);
+        if (params.isEmpty()) {
+            if (!url.contains("?")) {
+                sb.append("?");
+            } else {
+                sb.append('&');
+            }
+            int count = 0;
+            while (iterator.hasNext()) {
+                count++;
+                Map.Entry<String, String> entry = iterator.next();
+                String key = entry.getKey();
+                String value = entry.getValue();
+                if (count > 1) {
+                    sb.append("&");
+                }
+                sb.append(key).append("=").append(value);
+            }
+        }
+        return sb.toString();
+
+    }
+}
