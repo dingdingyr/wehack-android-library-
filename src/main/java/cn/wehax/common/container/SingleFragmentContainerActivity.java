c88f17090a4ce714545b38b5d733b9d96ee8d638
dusensong <58247052@qq.com>
2015-05-26 18:24:10 +0800

    add SingleFragmentContainerActivity

--- src/main/java/cn/wehax/common/container/SingleFragmentContainerActivity.java
+++ src/main/java/cn/wehax/common/container/SingleFragmentContainerActivity.java
@@ -9,18 +9,18 @@ import roboguice.activity.RoboFragmentActivity;
 /**
  * A BaseActivity that simply contains a single fragment. The intent
  * used to invoke this activity is forwarded to the fragment as arguments during
- * fragment instantiation. Derived activities should only need to implement onCreatePane()
+ * fragment instantiation. Derived activities should only need to implement onCreateFragment()
  */
-public abstract class SinglePaneFragmentContainer extends RoboFragmentActivity {
+public abstract class SingleFragmentContainerActivity extends RoboFragmentActivity {
     private Fragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_singlepane_empty);
+        setContentView(R.layout.activity_single_fragment);
 
         if (savedInstanceState == null) {
-            mFragment = onCreatePane();
+            mFragment = onCreateFragment();
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.root_container, mFragment)
                     .commit();
@@ -28,12 +28,12 @@ public abstract class SinglePaneFragmentContainer extends RoboFragmentActivity {
     }
 
     /**
-     * 获取onCreatePane方法传入的Single Pane
+     * 获取onCreatePane方法传入的Single Fragment
      * @return
      */
     protected Fragment getFragment(){
        return mFragment;
     }
 
-    protected abstract Fragment onCreatePane();
+    protected abstract Fragment onCreateFragment();
 }
